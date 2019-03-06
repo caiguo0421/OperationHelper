67ef0a6e8fe8e985ce3e40b86fb475fc26bbaf6a
caigx-pc <caiguo0421@163.com>
2019-03-06 12:04:46 +0800

    增加批量生成项目

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -28,19 +28,269 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tab_main = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tbpnl_main = new System.Windows.Forms.TableLayoutPanel();
+            this.txt_template = new System.Windows.Forms.TextBox();
+            this.lbl_tomcat = new System.Windows.Forms.Label();
+            this.txt_tomcatHome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_cvs = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gpb_log = new System.Windows.Forms.GroupBox();
+            this.txt_log = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_cvs = new System.Windows.Forms.Button();
+            this.cbb_sourceType = new System.Windows.Forms.ComboBox();
+            this.pnl_buttom = new System.Windows.Forms.Panel();
+            this.chb_force = new System.Windows.Forms.CheckBox();
+            this.btn_deploy = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tab_main.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tbpnl_main.SuspendLayout();
+            this.gpb_log.SuspendLayout();
+            this.pnl_buttom.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tab_main
+            // 
+            this.tab_main.Controls.Add(this.tabPage1);
+            this.tab_main.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tab_main.Location = new System.Drawing.Point(0, 0);
+            this.tab_main.Name = "tab_main";
+            this.tab_main.SelectedIndex = 0;
+            this.tab_main.Size = new System.Drawing.Size(994, 446);
+            this.tab_main.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.tbpnl_main);
+            this.tabPage1.Controls.Add(this.pnl_buttom);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(986, 420);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "文件替换";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tbpnl_main
+            // 
+            this.tbpnl_main.ColumnCount = 6;
+            this.tbpnl_main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 5F));
+            this.tbpnl_main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tbpnl_main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tbpnl_main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tbpnl_main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tbpnl_main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 5F));
+            this.tbpnl_main.Controls.Add(this.txt_template, 2, 1);
+            this.tbpnl_main.Controls.Add(this.lbl_tomcat, 1, 0);
+            this.tbpnl_main.Controls.Add(this.txt_tomcatHome, 2, 0);
+            this.tbpnl_main.Controls.Add(this.label1, 1, 2);
+            this.tbpnl_main.Controls.Add(this.txt_cvs, 2, 2);
+            this.tbpnl_main.Controls.Add(this.label3, 1, 1);
+            this.tbpnl_main.Controls.Add(this.gpb_log, 1, 4);
+            this.tbpnl_main.Controls.Add(this.button1, 4, 1);
+            this.tbpnl_main.Controls.Add(this.btn_cvs, 4, 2);
+            this.tbpnl_main.Controls.Add(this.cbb_sourceType, 1, 3);
+            this.tbpnl_main.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbpnl_main.Location = new System.Drawing.Point(3, 3);
+            this.tbpnl_main.Margin = new System.Windows.Forms.Padding(2);
+            this.tbpnl_main.Name = "tbpnl_main";
+            this.tbpnl_main.RowCount = 5;
+            this.tbpnl_main.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tbpnl_main.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tbpnl_main.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tbpnl_main.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tbpnl_main.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tbpnl_main.Size = new System.Drawing.Size(980, 380);
+            this.tbpnl_main.TabIndex = 12;
+            // 
+            // txt_template
+            // 
+            this.txt_template.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_template.Location = new System.Drawing.Point(158, 28);
+            this.txt_template.Name = "txt_template";
+            this.txt_template.Size = new System.Drawing.Size(674, 21);
+            this.txt_template.TabIndex = 9;
+            // 
+            // lbl_tomcat
+            // 
+            this.lbl_tomcat.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.lbl_tomcat.AutoSize = true;
+            this.lbl_tomcat.Location = new System.Drawing.Point(87, 6);
+            this.lbl_tomcat.Name = "lbl_tomcat";
+            this.lbl_tomcat.Size = new System.Drawing.Size(65, 12);
+            this.lbl_tomcat.TabIndex = 2;
+            this.lbl_tomcat.Text = "tomcat路径";
+            // 
+            // txt_tomcatHome
+            // 
+            this.txt_tomcatHome.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_tomcatHome.Location = new System.Drawing.Point(158, 3);
+            this.txt_tomcatHome.Name = "txt_tomcatHome";
+            this.txt_tomcatHome.Size = new System.Drawing.Size(674, 21);
+            this.txt_tomcatHome.TabIndex = 1;
+            this.txt_tomcatHome.Text = "D:\\Apache Software Foundation\\apache-tomcat-7.0.82";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "csv路径";
+            // 
+            // txt_cvs
+            // 
+            this.txt_cvs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_cvs.Location = new System.Drawing.Point(158, 53);
+            this.txt_cvs.Name = "txt_cvs";
+            this.txt_cvs.Size = new System.Drawing.Size(674, 21);
+            this.txt_cvs.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(75, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "项目模板路径";
+            // 
+            // gpb_log
+            // 
+            this.tbpnl_main.SetColumnSpan(this.gpb_log, 4);
+            this.gpb_log.Controls.Add(this.txt_log);
+            this.gpb_log.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gpb_log.Location = new System.Drawing.Point(8, 103);
+            this.gpb_log.Name = "gpb_log";
+            this.gpb_log.Size = new System.Drawing.Size(964, 274);
+            this.gpb_log.TabIndex = 12;
+            this.gpb_log.TabStop = false;
+            this.gpb_log.Text = "日志";
+            // 
+            // txt_log
+            // 
+            this.txt_log.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txt_log.Location = new System.Drawing.Point(3, 17);
+            this.txt_log.Multiline = true;
+            this.txt_log.Name = "txt_log";
+            this.txt_log.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_log.Size = new System.Drawing.Size(958, 254);
+            this.txt_log.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("宋体", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(895, 26);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 22);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "打开";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btn_cvs
+            // 
+            this.btn_cvs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_cvs.Font = new System.Drawing.Font("宋体", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_cvs.Location = new System.Drawing.Point(895, 51);
+            this.btn_cvs.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_cvs.Name = "btn_cvs";
+            this.btn_cvs.Size = new System.Drawing.Size(80, 22);
+            this.btn_cvs.TabIndex = 16;
+            this.btn_cvs.Text = "打开";
+            this.btn_cvs.UseVisualStyleBackColor = true;
+            // 
+            // cbb_sourceType
+            // 
+            this.cbb_sourceType.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbb_sourceType.FormattingEnabled = true;
+            this.cbb_sourceType.Location = new System.Drawing.Point(31, 78);
+            this.cbb_sourceType.Name = "cbb_sourceType";
+            this.cbb_sourceType.Size = new System.Drawing.Size(121, 20);
+            this.cbb_sourceType.TabIndex = 14;
+            // 
+            // pnl_buttom
+            // 
+            this.pnl_buttom.Controls.Add(this.chb_force);
+            this.pnl_buttom.Controls.Add(this.btn_deploy);
+            this.pnl_buttom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnl_buttom.Location = new System.Drawing.Point(3, 383);
+            this.pnl_buttom.Name = "pnl_buttom";
+            this.pnl_buttom.Size = new System.Drawing.Size(980, 34);
+            this.pnl_buttom.TabIndex = 11;
+            // 
+            // chb_force
+            // 
+            this.chb_force.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.chb_force.AutoSize = true;
+            this.chb_force.Location = new System.Drawing.Point(813, 9);
+            this.chb_force.Name = "chb_force";
+            this.chb_force.Size = new System.Drawing.Size(72, 16);
+            this.chb_force.TabIndex = 12;
+            this.chb_force.Text = "强制部署";
+            this.chb_force.UseVisualStyleBackColor = true;
+            // 
+            // btn_deploy
+            // 
+            this.btn_deploy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_deploy.Location = new System.Drawing.Point(891, 5);
+            this.btn_deploy.Name = "btn_deploy";
+            this.btn_deploy.Size = new System.Drawing.Size(75, 23);
+            this.btn_deploy.TabIndex = 0;
+            this.btn_deploy.Text = "部署";
+            this.btn_deploy.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(927, 431);
+            this.ClientSize = new System.Drawing.Size(994, 446);
+            this.Controls.Add(this.tab_main);
             this.Name = "Form3";
-            this.Text = "Form3";
+            this.Text = "服务端批量部署工具";
+            this.tab_main.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tbpnl_main.ResumeLayout(false);
+            this.tbpnl_main.PerformLayout();
+            this.gpb_log.ResumeLayout(false);
+            this.gpb_log.PerformLayout();
+            this.pnl_buttom.ResumeLayout(false);
+            this.pnl_buttom.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tab_main;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TableLayoutPanel tbpnl_main;
+        private System.Windows.Forms.TextBox txt_template;
+        private System.Windows.Forms.Label lbl_tomcat;
+        private System.Windows.Forms.TextBox txt_tomcatHome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_cvs;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox gpb_log;
+        private System.Windows.Forms.TextBox txt_log;
+        private System.Windows.Forms.ComboBox cbb_sourceType;
+        private System.Windows.Forms.Panel pnl_buttom;
+        private System.Windows.Forms.CheckBox chb_force;
+        private System.Windows.Forms.Button btn_deploy;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_cvs;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
--- Form3.cs
+++ Form3.cs
@@ -1,19 +1,304 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace OperationHelper
 {
     public partial class Form3 : Form
     {
+
+        private DataTable dt_csv = null;
+
+        private bool force = false;
+
+
+        private const string REGEX_WEB_XML = "<param-value>\\w.root</param-value>";
+
+        private const String REGEX_LOG_PROPERTIES = "{\\w.root}";
+
+        private const string FILE_APPLICATION = @"\WEB-INF\classes\application.properties";
+        private const string FILE_LOG4J_PROPERTIES = @"\WEB-INF\classes\log4j.properties";
+        private const string FILE_WEB = @"\WEB-INF\web.xml";
+
         public Form3()
         {
             InitializeComponent();
+            InitConfig();
+            InitCbb();
+            txt_cvs.TextChanged += Txt_cvs_TextChanged;
+            btn_cvs.Click += Btn_cvs_Click;
+            btn_deploy.Click += Btn_deploy_Click;
+        }
+
+        private void InitConfig()
+        {
+            this.txt_tomcatHome.Text = ConfigurationManager.AppSettings["tomcatHome"];
+        }
+
+        private void InitCbb()
+        {
+            string[] array = { "简版", "完整版" };
+            this.cbb_sourceType.DataSource = array;
+            this.cbb_sourceType.SelectedIndex = 0;
+        }
+
+
+        private void Btn_cvs_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txt_cvs.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void Txt_cvs_TextChanged(object sender, EventArgs e)
+        {
+            string cvsFilePath = txt_cvs.Text;
+            if (String.IsNullOrEmpty(cvsFilePath))
+            {
+                return;
+            }
+
+            dt_csv = CSVFileHelper.OpenCSV(cvsFilePath);
+        }
+
+        private void Btn_deploy_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("替换文件前请做好备份，确定替换文件吗？", "部署", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            if (result != DialogResult.OK)
+                return;
+
+            try
+            {
+                DoDeploy();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("程序出现了问题：" + ex.Message);
+                return;
+            }
+            MessageBox.Show("成功");
+        }
+
+        /// <summary>
+        /// 部署
+        /// </summary>
+        private void DoDeploy()
+        {
+            if (String.IsNullOrEmpty(txt_tomcatHome.Text))
+            {
+                throw new Exception("tomcat路径为空");
+            }
+
+            if (string.IsNullOrEmpty(txt_template.Text))
+            {
+                throw new Exception("模板路径为空");
+            }
+
+            if (string.IsNullOrEmpty(txt_cvs.Text))
+            {
+                throw new Exception("cvs路径为空");
+            }
+
+            if (dt_csv == null || dt_csv.Rows.Count == 0)
+            {
+                throw new Exception("cvs内容为空");
+            }
+
+
+            if (!Directory.Exists(txt_tomcatHome.Text))//判断是否存在
+            {
+                throw new Exception("tomcat路径不存在");
+            }
+
+            DirectoryInfo dicWebapp = new DirectoryInfo(txt_tomcatHome.Text + "\\webapps");
+            if (!dicWebapp.Exists)
+            {
+                throw new Exception(dicWebapp.FullName + "不存在");
+            }
+
+            DirectoryInfo dicTemplate = new DirectoryInfo(txt_template.Text);
+            if (!dicTemplate.Exists)
+            {
+                throw new Exception("项目模板不存在");
+            }
+
+            force = chb_force.Checked;
+            DoCopy(dicWebapp, dicTemplate);
+        }
+
+        /// <summary>
+        /// 复制项目
+        /// </summary>
+        /// <param name="dicWebapp"></param>
+        /// <param name="dicTemplate"></param>
+        private void DoCopy(DirectoryInfo dicWebapp, DirectoryInfo dicTemplate)
+        {
+            foreach (DataRow dr in dt_csv.Rows)
+            {
+                string companyNo = dr["company_no"] + "";
+                if (string.IsNullOrEmpty(companyNo))
+                {
+                    continue;
+                }
+
+                DirectoryInfo dic_target = new DirectoryInfo(dicWebapp.FullName + "\\" + companyNo);
+                if (!force && dic_target.Exists)
+                {
+                    //如果项目已经存在而且不是强制更新，跳过
+                    continue;
+                }
+
+                if (dic_target.Exists)
+                {
+                    //备份
+                    //TODO
+                    dic_target.Delete(true);//删除原来已有的
+                }
+            }
+        }
+
+        private static void CopyDirectory(DirectoryInfo dicTemplate, DirectoryInfo dic_target, DataRow dr_csv)
+        {
+            if (!dicTemplate.Exists)
+            {
+                throw new Exception(string.Format("{0}不存在", dicTemplate.FullName));
+            }
+
+            if (!dic_target.Exists)
+            {
+                dic_target.Create();
+            }
+
+            foreach (FileInfo fi in dicTemplate.GetFiles())
+            {
+                fi.CopyTo(dic_target.FullName + fi.Name, true);
+            }
+
+            foreach (DirectoryInfo di in dicTemplate.GetDirectories())
+            {
+                CopyDirectory(di, new DirectoryInfo(dic_target.FullName + di.Name), dr_csv);
+            }
+        }
+
+       
+
+        private static void CopyFile(FileInfo fiTemplate, DirectoryInfo dic_target, DataRow dr_csv)
+        {
+            String file_target = dic_target.FullName + fiTemplate.Name;
+            fiTemplate.CopyTo(file_target, true);
+            string text = string.Empty;
+            using (FileStream fs = new FileStream(fiTemplate.FullName, FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    text = sr.ReadToEnd();
+                }
+            }
+
+            using (FileStream fs = new FileStream(file_target, FileMode.Create, FileAccess.Write))
+            {
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    if (file_target.EndsWith(FILE_APPLICATION))
+                    {
+                        if (text.Contains("jdbc.url"))
+                        {
+                            text = string.Format("jdbc.url={0}", dr_csv["datasource_url"] + "DatabaseName=" + dr_csv["datasource_db_name"]);
+                        }
+                        else if (text.Contains("jdbc.username"))
+                        {
+                            text = string.Format("jdbc.username={0}", dr_csv["datasource_username"]);
+                        }
+                        else if (text.Contains("jdbc.password"))
+                        {
+                            text = string.Format("jdbc.password={0}", dr_csv["datasource_password"]);
+                        }
+                        else if (text.Contains("ftp.hostname"))
+                        {
+                            text = string.Format("ftp.hostname={0}", dr_csv["ftp_hostname"]);
+                        }
+                        else if (text.Contains("ftp.port"))
+                        {
+                            text = string.Format("ftp.port={0}", dr_csv["ftp_port"]);
+                        }
+                        else if (text.Contains("ftp.username"))
+                        {
+                            text = string.Format("ftp.username={0}", dr_csv["ftp_username"]);
+                        }
+                        else if (text.Contains("ftp.password"))
+                        {
+                            text = string.Format("ftp.password={0}", dr_csv["ftp_password"]);
+                        }
+                        else if (text.Contains("ftp.attachment.path"))
+                        {
+                            text = string.Format("ftp.attachment.path={0}", dr_csv["ftp_attachment_path"]);
+                        }
+                    }
+                    else if (file_target.EndsWith(FILE_LOG4J_PROPERTIES))
+                    {
+                        if(Regex.IsMatch(text, REGEX_LOG_PROPERTIES))
+                        {
+                            text = Regex.Replace(text, REGEX_LOG_PROPERTIES, string.Format("{0}.root", dr_csv["company_no"]));
+                        }
+                    }
+                    else if (file_target.EndsWith(FILE_WEB))
+                    {
+                        if(Regex.IsMatch(text, REGEX_WEB_XML))
+                        {
+                            text = string.Format("<param-value>{0}.root</param-value>", dr_csv["company_no"]);
+                        }
+                       
+                    }
+
+                    sw.Write(text);
+                }
+            }
+        }
+
+
+        private static bool CopyDirectory(string SourcePath, string DestinationPath, bool overwriteexisting)
+        {
+            bool ret = false;
+            try
+            {
+                SourcePath = SourcePath.EndsWith(@"\") ? SourcePath : SourcePath + @"\";
+                DestinationPath = DestinationPath.EndsWith(@"\") ? DestinationPath : DestinationPath + @"\";
+
+                if (Directory.Exists(SourcePath))
+                {
+                    if (Directory.Exists(DestinationPath) == false)
+                    {
+                        Directory.CreateDirectory(DestinationPath);
+                    }
+
+                    foreach (string fls in Directory.GetFiles(SourcePath))
+                    {
+                        FileInfo flinfo = new FileInfo(fls);
+                        flinfo.CopyTo(DestinationPath + flinfo.Name, overwriteexisting);
+                    }
+                    foreach (string drs in Directory.GetDirectories(SourcePath))
+                    {
+                        DirectoryInfo drinfo = new DirectoryInfo(drs);
+                        if (CopyDirectory(drs, DestinationPath + drinfo.Name, overwriteexisting) == false)
+                            ret = false;
+                    }
+                }
+                ret = true;
+            }
+            catch (Exception ex)
+            {
+                ret = false;
+            }
+            return ret;
         }
     }
 }
